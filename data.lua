dcbb573417a86009ee3896a9dcce24ca6b870030
Killashandra Scheuring <killashandra@sylvia.com>
2022-08-14 12:10:31 -0500

    Initial commit. Got bug egg that makes friendly biter.

---
+++ data.lua
@@ -0,0 +1,12 @@
+require("prototypes.entity")
+require("prototypes.item")
+require("prototypes.recipes")
+
+data:extend({
+    {
+  type = "item-subgroup",
+  name = "biter-egg",
+  group = "production",
+  order = "b-b-a"
+}
+})
